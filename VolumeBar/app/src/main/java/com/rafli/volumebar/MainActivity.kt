6d8f13ae1d4910af7a388e2f39bf20b6db919847
Kavarera <rafli.030715@gmail.com>
2021-05-28 19:59:07 +0700

    Dicoding VolumeBar 27 Mei 2021

---
+++ VolumeBar/app/src/main/java/com/rafli/volumebar/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.rafli.volumebar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
+    companion object {
+        private const val STATE_RESULT = "state_result"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        edtWidth = findViewById(R.id.edt_width)
+        edtLength = findViewById(R.id.edt_length)
+        edtHeight = findViewById(R.id.edt_height)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+
+        btnCalculate.setOnClickListener(this)
+
+        if (savedInstanceState !=null){
+            val result = savedInstanceState.getString(STATE_RESULT)
+            tvResult.text = result
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_calculate){
+            val inputLength = edtLength.text.toString().trim()
+            val inputWidth = edtWidth.text.toString().trim()
+            val inputHeight = edtHeight.text.toString().trim()
+
+            var isEmptyFields = false
+
+            if(inputHeight.isEmpty()){
+                isEmptyFields = true
+                edtHeight.error = "It cant be empty!"
+
+            }
+
+            if(inputWidth.isEmpty()){
+                isEmptyFields = true
+                edtWidth.error = "It cant be empty!"
+
+            }
+
+            if(inputLength.isEmpty()){
+                isEmptyFields = true
+                edtLength.error = "It cant be empty!"
+
+            }
+
+            if (!isEmptyFields){
+                val volume = inputHeight.toDouble() * inputWidth.toDouble() * inputLength.toDouble()
+                tvResult.text = volume.toString()
+            }
+
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT,tvResult.text.toString())
+    }
+}
