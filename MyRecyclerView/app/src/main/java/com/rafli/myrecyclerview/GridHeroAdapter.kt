665f8a0c488819ed86ac18d8615c194650686e53
Kavarera <rafli.030715@gmail.com>
2021-06-01 00:48:41 +0700

    RecycleView, Gettin lost on it. gonna find another tutorial on youtube and searching on SOF.

---
+++ MyRecyclerView/app/src/main/java/com/rafli/myrecyclerview/GridHeroAdapter.kt
@@ -0,0 +1,25 @@
+package com.rafli.myrecyclerview
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class GridHeroAdapter(val listHero:ArrayList<Hero>): RecyclerView.Adapter<GridHeroAdapter.GridViewHolder> {
+    inner class GridViewHolder {
+
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): GridHeroAdapter.GridViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: GridHeroAdapter.GridViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
